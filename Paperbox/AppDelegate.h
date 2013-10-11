74c5ac214523855c1fb9219eb01d26a947f5196e
Andrey M <meamka@me.com>
2013-10-11 20:48:46 +0400

    Initial Commit

---
+++ Paperbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paperbox
+//
+//  Created by Andrey M on 11.10.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Paperbox/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Paperbox
+//
+//  Created by Andrey M on 11.10.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
